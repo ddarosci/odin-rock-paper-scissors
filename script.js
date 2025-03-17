91c1977c574f8b8f771f7cd463256e6bbb4b62d6
diogo darosci <diogodarosci123@gmail.com>
2025-03-16 23:38:34 -0300

    added playgame function

--- script.js
+++ script.js
@@ -17,43 +17,54 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-    switch(humanChoice) {
-        case "rock":
-            if(computerChoice === "paper") {
-                console.log("You lose! Paper beats rock");
-                computerScore++;
-            }
-            else if(computerChoice === "scissors") {
-                console.log("You win! Rock beats scissors");
-                humanScore++;
-            } else {
-                console.log("It's a draw! You both choose the same object");
-            }
-            break;
-        case "paper":
-            if(computerChoice === "rock") {
-                console.log("You win! Paper beats rock");
-                humanScore++;
-            } else if(computerChoice === "scissors") {
-                console.log("You lose! Scissors beats paper");
-                computerScore++;
-            } else {
-                console.log("It's a draw! You both choose the same object");
-            }
-            break;
-        case "scissors":
-            if(computerChoice === "rock") {
-                console.log("You lose! Rock beats scissors");
-                computerScore++;
-            } else if(computerChoice === "paper") {
-                console.log("You win! Scissors beats paper");
-                humanScore++;
-            } else {
-                console.log("It's a draw! You both choose the same object");
-            }
-            break;
-            default:
-                console.log("You did not pass the functions as arguments")
+function playGame() {
+    for(let i = 0; i < 5; i++) {
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        switch(humanChoice) {
+            case "rock":
+                if(computerChoice === "paper") {
+                    console.log("You lose! Paper beats rock");
+                    computerScore++;
+                }
+                else if(computerChoice === "scissors") {
+                    console.log("You win! Rock beats scissors");
+                    humanScore++;
+                } else {
+                    console.log("It's a draw! You both choose the same object");
+                }
+                break;
+            case "paper":
+                if(computerChoice === "rock") {
+                    console.log("You win! Paper beats rock");
+                    humanScore++;
+                } else if(computerChoice === "scissors") {
+                    console.log("You lose! Scissors beats paper");
+                    computerScore++;
+                } else {
+                    console.log("It's a draw! You both choose the same object");
+                }
+                break;
+            case "scissors":
+                if(computerChoice === "rock") {
+                    console.log("You lose! Rock beats scissors");
+                    computerScore++;
+                } else if(computerChoice === "paper") {
+                    console.log("You win! Scissors beats paper");
+                    humanScore++;
+                } else {
+                    console.log("It's a draw! You both choose the same object");
+                }
+                break;
+                default:
+                    console.log("You did not pass the functions as arguments")
+        }
+    }
+    if(humanScore > computerScore) {
+        console.log("Congratulations! You won!")
+    } else if (humanScore < computerScore) {
+        console.log("You lost! Better luck next time!")
+    } else {
+        console.log("It's a draw")
     }
 }
