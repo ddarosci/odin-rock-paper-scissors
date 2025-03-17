912e685358fcfd0a33de64d594a519779c0adc00
diogo darosci <diogodarosci123@gmail.com>
2025-03-16 22:57:10 -0300

    single round function

--- script.js
+++ script.js
@@ -11,5 +11,49 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let humanChoice = prompt("Choose between rock, paper, or scissors: ");
-    return humanChoice;
+    return humanChoice.toLowerCase();
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    switch(humanChoice) {
+        case "rock":
+            if(computerChoice === "paper") {
+                console.log("You lose! Paper beats rock");
+                computerScore++;
+            }
+            else if(computerChoice === "scissors") {
+                console.log("You win! Rock beats scissors");
+                humanScore++;
+            } else {
+                console.log("It's a draw! You both choose the same object");
+            }
+            break;
+        case "paper":
+            if(computerChoice === "rock") {
+                console.log("You win! Paper beats rock");
+                humanScore++;
+            } else if(computerChoice === "scissors") {
+                console.log("You lose! Scissors beats paper");
+                computerScore++;
+            } else {
+                console.log("It's a draw! You both choose the same object");
+            }
+            break;
+        case "scissors":
+            if(computerChoice === "rock") {
+                console.log("You lose! Rock beats scissors");
+                computerScore++;
+            } else if(computerChoice === "paper") {
+                console.log("You win! Scissors beats paper");
+                humanScore++;
+            } else {
+                console.log("It's a draw! You both choose the same object");
+            }
+            break;
+            default:
+                console.log("You did not pass the functions as arguments")
+    }
 }
